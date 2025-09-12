c5734c9d206792d6074714b0f74026827366ae74
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-09-12 23:58:30 +0500

    Fixed responsiveness issue

--- Jobdemo/src/components/HeroSection/HeroSection.jsx
+++ Jobdemo/src/components/HeroSection/HeroSection.jsx
@@ -58,7 +58,24 @@ export default function HeroSection() {
           </div>
         </header>
 
-        <div className="flex justify-center">
+        <div className="relative flex flex-col justify-between h-[80vh] md:hidden">
+        <div className="mt-12 px-12 font-semibold">
+            <h1 className="text-5xl sm:text-6xl">
+            Note Taking
+            </h1>
+            <h1 className="text-5xl sm:text-6xl text-[#00BB77]">
+            Redefined
+            </h1>
+        </div>
+        <div className="flex justify-center items-center flex-grow">
+            <img
+            src="/octopusMobile.png"
+            alt="CirriNote mascot"
+            className="w-full h-96 absolute right-0 object-cover"
+            />
+        </div>
+        </div>
+        <div className="hidden md:flex justify-center items-end h-full">
           <img
             src="/Octopus.png"
             alt="CirriNote mascot"
