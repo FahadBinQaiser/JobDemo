4ac5d38dc3c94f0141fb94343d6c82b5d01649b9
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-09-13 00:35:26 +0500

    Fixing

--- Jobdemo/src/components/Note/Note.jsx
+++ Jobdemo/src/components/Note/Note.jsx
@@ -2,14 +2,7 @@ import React from "react";
 export default function Note() {
   return (
     <section
-    className="min-h-screen flex flex-col items-center justify-center px-6 py-20 relative"
-    style={{
-      backgroundImage: "url('/Section.png')",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    }}
-  >
+    className="min-h-screen flex flex-col items-center justify-center px-6 py-20 relative">
     <div className="absolute inset-0 bg-black/90 bg-opacity-60"></div>
 
     <div className="max-w-4xl mx-auto text-center space-y-12 relative z-10">
@@ -25,7 +18,7 @@ export default function Note() {
         </div>
       </div>
 
-      <p className="text-lg md:text-xl text-white max-w-2xl mx-auto">
+      <p className="text-xl md:text-xl text-white max-w-2xl mx-auto">
         Creators around the planet use this app
         <br />
         for creating <span className="text-green-400">magic</span>
