cc1c30d497bcb565c00b763fc7429b5d9eb23442
Fahad <fahadbinqaiser3@gmail.com>
2025-09-13 13:37:17 +0500

    Changing stuff

--- Jobdemo/src/components/Note/Note.jsx
+++ Jobdemo/src/components/Note/Note.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 export default function Note() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center px-6 py-20 relative">
+    <section className="h-full flex flex-col items-center justify-center px-6 py-20 relative">
       <div className="absolute inset-0 bg-black/90 bg-opacity-60"></div>
 
       <div className="max-w-4xl mx-auto text-center space-y-12 relative z-10">
         <h2 className="text-3xl md:text-6xl lg:text-7xl font-semibold text-white leading-tight">
-          An extraordinary note
-          <br />
-          for <span className="text-green-400">makers, creators..</span>
+          An extraordinary note{" "}
+          <span className="sm:block">
+            for <span className="text-green-400">makers, creators..</span>
+          </span>
         </h2>
 
         <div className="flex justify-center">
